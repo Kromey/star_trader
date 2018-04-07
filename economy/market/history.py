9d9ed6b5750072efe566be679fc71633cb71a2a1
Travis Veazey <travisvz@gmail.com>
2018-04-06 16:43:06 -0800

    MarketHistory will now log warnings if days are not opened/closed

--- economy/market/history.py
+++ economy/market/history.py
@@ -1,7 +1,11 @@
 from collections import namedtuple
+import logging
 
 
 from economy import goods
+
+
+logger = logging.getLogger(__name__)
 
 
 Trades = namedtuple('Trades', ['volume', 'low', 'high', 'mean'])
@@ -11,17 +15,24 @@ class MarketHistory(object):
     def __init__(self, max_depth=30):
         self._max_depth = max_depth
         self._history = []
-        self._day = []
+        self._day = None
 
     def open_day(self):
+        if self._day is not None:
+            logger.warning('Opening new day before previous day was properly closed. Its trades have been lost. You must call close_day() to commit trades to the history.')
+
         self._day = []
 
     def add_trades(self, good, trades):
+        if self._day is None:
+            logger.warning('Implicitly opening a new day to record this trade. You should call open_day() yourself.')
+            self.open_day()
+
         self._day.append((good, trades))
 
     def close_day(self):
         self._history.append(tuple(self._day))
-        self._day = []
+        self._day = None
 
         # Be lazy about our garbage collection
         if len(self._history) > 1.5 * self._max_depth:
@@ -29,6 +40,9 @@ class MarketHistory(object):
 
     @property
     def history(self, depth=None):
+        if self._day is not None:
+            logger.warning('Day has been left open. It will not appear in the history.')
+
         if depth is None:
             depth = self._max_depth
 
